62aec1442f0e8a55f768e673ddc2d658dee40777
GabrielJospin <gabrielmjospin@gmail.com>
2025-12-17 11:06:27 -0300

    Remove file logger

--- src/main.py
+++ src/main.py
@@ -13,19 +13,14 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 c_handler = logging.StreamHandler(sys.stdout) 
-f_handler = logging.FileHandler('erros.log')  
 
 c_handler.setLevel(logging.INFO)  
-f_handler.setLevel(logging.ERROR)
 
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
 
 logger.addHandler(c_handler)
-logger.addHandler(f_handler)
 
 
 ### ROTAS DA API
